8ef88e7b61b9d557d281adf061b097be6c26b5ad
Esteban Higuita <esteban.higuita@ceiba.com.co>
2019-09-20 16:25:23 -0500

    Android: Agregar actividad para splash

---
+++ MonkeyVehicleShop.Droid/SplashActivity.cs
@@ -0,0 +1,19 @@
+﻿
+using Android.App;
+using Android.Content;
+using Android.OS;
+
+namespace MonkeyVehicleShop.Droid
+{
+    [Activity(MainLauncher = true)]
+    public class SplashActivity : Activity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            Intent intent = new Intent(this, typeof(MainActivity));
+            StartActivity(intent);
+        }
+    }
+}
