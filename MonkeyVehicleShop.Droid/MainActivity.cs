53533012fa6a708268fa116f97cb9ec959f8257a
David Santacoloma <adsi1409@hotmail.com>
2019-09-19 19:11:29 -0500

    Gral: Agregar proyecto Android

---
+++ MonkeyVehicleShop.Droid/MainActivity.cs
@@ -0,0 +1,18 @@
+﻿using Android.App;
+using Android.OS;
+using Android.Support.V7.App;
+
+namespace MonkeyVehicleShop.Droid
+{
+    [Activity(MainLauncher = true)]
+    public class MainActivity : AppCompatActivity
+    {
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            SetContentView(Resource.Layout.activity_main);
+        }
+    }
+}
+
