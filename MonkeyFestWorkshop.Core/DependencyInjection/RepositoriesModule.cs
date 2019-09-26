ce017d9d0a3f73c42c88ad7add42690769c3a7c8
David Santacoloma <adsi1409@hotmail.com>
2019-09-26 07:53:54 -0500

    Gral: Agregar contenedor para inyección de dependencias al Core

---
+++ MonkeyFestWorkshop.Core/DependencyInjection/RepositoriesModule.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Autofac;
+
+namespace MonkeyFestWorkshop.Core.DependencyInjection
+{
+    public class RepositoriesModule: Module
+    {
+        protected override void Load(ContainerBuilder builder)
+        {
+            base.Load(builder);
+        }
+    }
+}
---
+++ MonkeyFestWorkshop.Core/DependencyInjection/SetupContainer.cs
@@ -0,0 +1,19 @@
+﻿using Autofac;
+
+namespace MonkeyFestWorkshop.Core.DependencyInjection
+{
+    public class SetupContainer
+    {
+        public IContainer CreateContainer()
+        {
+            var containerBuilder = new ContainerBuilder();
+            RegisterDependencies(containerBuilder);
+            return containerBuilder.Build();
+        }
+
+        private void RegisterDependencies(ContainerBuilder containerBuilder)
+        {
+            containerBuilder.RegisterModule<RepositoriesModule>();
+        }
+    }
+}
