8df06ff93a8efc95fa883b4290b212075564e25f
David Santacoloma <adsi1409@hotmail.com>
2019-09-26 17:58:49 -0500

    Gral: Crear repositorio para los vehículos e implementación mock vacía

---
+++ MonkeyFestWorkshop.DataAccess/Implementations/Mock/VehicleRepositoryMock.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using MonkeyFestWorkshop.DataAccess.Repositories;
+using MonkeyFestWorkshop.Domain.Models;
+
+namespace MonkeyFestWorkshop.DataAccess.Implementations.Mock
+{
+    public class VehicleRepositoryMock : IVehicleRepository<BaseVehicle>
+    {
+        private readonly List<BaseVehicle> vehicles;
+
+        public VehicleRepositoryMock()
+        {
+            vehicles = new List<BaseVehicle>();
+
+            var car = new Car();
+            car.Id = "1";
+            car.BrandName = "Mazda";
+            car.Line = "3";
+            car.Model = "2019";
+            car.Plate = "XXX111";
+            vehicles.Add(car);
+
+            var motorcycle = new Motorcycle();
+            motorcycle.Id = "2";
+            vehicles.Add(motorcycle);
+        }
+
+        public IEnumerable<BaseVehicle> GetAllVehicles()
+        {
+            return vehicles;
+        }
+
+        public BaseVehicle GetVehicleById(string id)
+        {
+            return vehicles.FirstOrDefault(x => x.Id.Equals(id));
+        }
+    }
+}
---
+++ MonkeyFestWorkshop.DataAccess/Repositories/IVehicleRepository.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using MonkeyFestWorkshop.Domain.Models;
+
+namespace MonkeyFestWorkshop.DataAccess.Repositories
+{
+    public interface IVehicleRepository<T> where T : BaseVehicle
+    {
+        IEnumerable<T> GetAllVehicles();
+
+        T GetVehicleById(string id);
+    }
+}
