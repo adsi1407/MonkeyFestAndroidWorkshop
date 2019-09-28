c1e7461a38522bf9313ffd61401522ed9c5d202a
David Santacoloma <adsi1409@hotmail.com>
2019-09-28 09:14:32 -0500

    Gral: Agregar ServiceDomain para manejo de usuarios

--- MonkeyFestWorkshop.Core/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace MonkeyFestWorkshop.Core
-{
-    public class Class1
-    {
-    }
-}
---
+++ MonkeyFestWorkshop.Core/DomainServices/UserServiceDomain.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using MonkeyFestWorkshop.DataAccess.Repositories;
+using MonkeyFestWorkshop.Domain.Models.User;
+
+namespace MonkeyFestWorkshop.Core.DomainServices
+{
+    public class UserServiceDomain
+    {
+        private readonly IUserRepository userRepository;
+
+        public UserServiceDomain(IUserRepository userRepository)
+        {
+            this.userRepository = userRepository;
+        }
+
+        public UserInfo GetUserInfo(string id)
+        {
+            return userRepository.GetUserInfo(id);
+        }
+    }
+}
